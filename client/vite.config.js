c2efd4d558c782b2515a60a1a7ece0bae4435692
Satyam Gupta <satyamg501@gmail.com>
2025-06-04 22:33:50 +0530

    changes Done

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.match(/react|react-dom/)) {
+              return 'react-vendor'
+            }
+            return 'vendor'
+          }
+        }
+      }
+    }
+  }
 })
